b78d5d7414585007b6f6fcbe62cb2c38c72b5fee
unknown <zhuel.te@gmail.com>
2026-03-04 23:46:14 -0500

    ultima actualizacion

--- app/src/main/java/com/example/appcine/Principal.java
+++ app/src/main/java/com/example/appcine/Principal.java
@@ -1,6 +1,9 @@
 package com.example.appcine;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Principal extends AppCompatActivity {
+    Button btnHarry1,btntoystory,btnavatar1;
+    private void loadUi() {
+        btnHarry1 = findViewById(R.id.btnHarry1);
+        btntoystory=findViewById(R.id.btntoystory);
+        btnavatar1=findViewById((R.id.btnavatar1));
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,28 @@ public class Principal extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        loadUi();
+        btnHarry1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), pelicula1.class);
+                startActivity(intent);
+            }
+        });
+        btntoystory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), pelicula3.class);
+                startActivity(intent);
+            }
+        });
+        btnavatar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), pelicula2.class);
+                startActivity(intent);
+            }
         });
     }
 }
