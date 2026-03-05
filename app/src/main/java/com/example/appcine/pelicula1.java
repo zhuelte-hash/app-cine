1b09791b2dd5dbaa0636f0bc59fae9e557e1d16b
unknown <zhuel.te@gmail.com>
2026-03-04 21:35:13 -0500

    Primer avance de AppCine

--- app/src/main/java/com/example/appcine/pelicula1.java
+++ app/src/main/java/com/example/appcine/pelicula1.java
@@ -1,39 +1,24 @@
 package com.example.appcine;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.content.Intent;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnReservar;
-    private void loadUi() {
-        btnReservar = findViewById(R.id.btnReservar);
-    }
+public class pelicula1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pelicula1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        loadUi();
-        btnReservar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Principal.class);
-                startActivity(intent);
-            }
         });
     }
 }
